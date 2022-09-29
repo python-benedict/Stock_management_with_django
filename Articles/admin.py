a595c3794525b26f50d069acfba9a0d1422a8dbb
Benedict <newlyf38@gmail.com>
2022-09-29 12:35:23 +0000

    added ArticleAdmin to the admin file so that i can add a list_display and a search fields

--- Articles/admin.py
+++ Articles/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Articles)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']    
+    search_fields = ['title', 'content']
+
+admin.site.register(Articles, ArticleAdmin)
--- Articles/models.py
+++ Articles/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Articles(models.Model):
     title = models.TextField()
     content = models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
